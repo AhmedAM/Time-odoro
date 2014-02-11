513be5dffab2abb077d42d07360604a2d168624e
Ahmed.A.M <ahmy-am@hotmail.com>
2014-02-11 18:56:44 +0300

    First Commit

---
+++ StickyNotes/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace StickyNotes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bt_add = new System.Windows.Forms.Button();
+            this.bt_save = new System.Windows.Forms.Button();
+            this.bt_edit = new System.Windows.Forms.Button();
+            this.bt_close = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // bt_add
+            // 
+            this.bt_add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_add.Font = new System.Drawing.Font("PF BeauSans Pro XThin", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_add.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bt_add.Location = new System.Drawing.Point(1, 1);
+            this.bt_add.Name = "bt_add";
+            this.bt_add.Size = new System.Drawing.Size(48, 30);
+            this.bt_add.TabIndex = 1;
+            this.bt_add.Text = "+";
+            this.bt_add.UseVisualStyleBackColor = true;
+            this.bt_add.Click += new System.EventHandler(this.bt_add_Click);
+            // 
+            // bt_save
+            // 
+            this.bt_save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_save.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bt_save.Location = new System.Drawing.Point(99, 1);
+            this.bt_save.Name = "bt_save";
+            this.bt_save.Size = new System.Drawing.Size(48, 30);
+            this.bt_save.TabIndex = 2;
+            this.bt_save.Text = "S";
+            this.bt_save.UseVisualStyleBackColor = true;
+            this.bt_save.Click += new System.EventHandler(this.bt_save_Click);
+            // 
+            // bt_edit
+            // 
+            this.bt_edit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_edit.Font = new System.Drawing.Font("PF BeauSans Pro XThin", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_edit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bt_edit.Location = new System.Drawing.Point(50, 1);
+            this.bt_edit.Name = "bt_edit";
+            this.bt_edit.Size = new System.Drawing.Size(48, 30);
+            this.bt_edit.TabIndex = 1;
+            this.bt_edit.Text = "E";
+            this.bt_edit.UseVisualStyleBackColor = true;
+            this.bt_edit.Click += new System.EventHandler(this.bt_edit_Click);
+            // 
+            // bt_close
+            // 
+            this.bt_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_close.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bt_close.Location = new System.Drawing.Point(149, 1);
+            this.bt_close.Name = "bt_close";
+            this.bt_close.Size = new System.Drawing.Size(48, 30);
+            this.bt_close.TabIndex = 2;
+            this.bt_close.Text = "X";
+            this.bt_close.UseVisualStyleBackColor = true;
+            this.bt_close.Click += new System.EventHandler(this.bt_close_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(197, 396);
+            this.ControlBox = false;
+            this.Controls.Add(this.bt_close);
+            this.Controls.Add(this.bt_save);
+            this.Controls.Add(this.bt_edit);
+            this.Controls.Add(this.bt_add);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_add;
+        private System.Windows.Forms.Button bt_save;
+        private System.Windows.Forms.Button bt_edit;
+        private System.Windows.Forms.Button bt_close;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ StickyNotes/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace StickyNotes
+{
+
+    public partial class Form1 : Form
+    {
+        //Mouse Drag Move start
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd,
+                         int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        //Mouse Drag Move Stop
+
+
+        
+        int TaskCounter  =2;
+        //int ButtonIndex = 3;
+        int TextBoxlocation = 52;
+        //int ButtonLocation = 70;
+        
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //timer1.Start();
+            //this.TopMost = true;
+        }
+
+
+
+        private void bt_add_Click(object sender, EventArgs e)
+        {
+            TextBox txt = new TextBox();
+            txt.Name = "TextBox" + TaskCounter;
+            
+            
+            txt.Size = new System.Drawing.Size(159,40);
+            txt.Multiline = true;
+            txt.Location = new Point(1, TextBoxlocation);
+            txt.Leave += new EventHandler(txt_Leave);
+            txt.DoubleClick += new EventHandler(txt_DoubleClick);
+            txt.TabIndex = TaskCounter;
+            txt.BackColor = System.Drawing.Color.LightSkyBlue;
+            //txt.BorderStyle = BorderStyle.None;
+            txt.ForeColor = Color.White;
+            txt.Font = new Font ("Georgia",10);
+            
+
+            Button bt = new Button();
+            bt.Name = "Button" + TaskCounter;
+            bt.TabIndex = TaskCounter;
+            bt.Size = new System.Drawing.Size(35, 38);
+            bt.Location = new Point(161, TextBoxlocation);
+            bt.BackColor = System.Drawing.Color.LightSkyBlue;
+            bt.FlatStyle = FlatStyle.Flat;
+            bt.ForeColor = System.Drawing.Color.White;
+            bt.Font = new Font("Wingdings", 15);
+            bt.Click += new EventHandler(bt_Click);
+            bt.Text = "þ";  // check box 
+
+            this.Controls.Add(txt);
+            this.Controls.Add(bt);
+            
+            
+            TaskCounter++;
+            TextBoxlocation += 38;
+           
+
+            //this.Controls.Remove(textBox1);
+        }
+
+        void bt_Click(object sender, EventArgs e)
+        {
+            int n = ((System.Windows.Forms.Control)(sender)).TabIndex;
+            var Txt = new Control[1]; 
+            Txt = this.Controls.Find("TextBox" + n, true);
+            Txt[0].Font = new Font(Txt[0].Font, FontStyle.Strikeout);
+            Txt[0].Text += Environment.NewLine+"Nailed It!";
+            
+
+
+            //this.Controls.Remove(Txt[0]);
+        }
+
+        void txt_DoubleClick(object sender, EventArgs e)
+        {
+
+            foreach (TextBox txt in this.Controls.OfType<TextBox>())
+            {
+                if (txt.ReadOnly) 
+                txt.ReadOnly=false;
+                
+            }
+           
+            
+//            throw new NotImplementedException();
+        }
+
+
+        void txt_Leave(object sender, EventArgs e)
+        {//use this function to update when it is left    Use tab index as ID 
+           // MessageBox.Show("banana");
+            //((System.Windows.Forms.Control)(sender)).Enabled = false ;
+
+
+            foreach (TextBox txt in this.Controls.OfType<TextBox>())
+            {
+                if (!(txt.Text == "") || !(txt.Text == " "))
+                    txt.ReadOnly = true;
+
+            }
+            
+            
+            //throw new NotImplementedException();
+        }
+
+        private void bt_edit_Click(object sender, EventArgs e)
+        {
+              foreach (TextBox txt in this.Controls.OfType<TextBox>())
+            {
+                if (txt.ReadOnly == true&& !(txt.Text.Contains("Nailed"))) 
+                txt.ReadOnly= false;
+                  
+            }
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            bt_add.Focus();
+        }
+
+        private void bt_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void bt_save_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (this.WindowState == FormWindowState.Minimized)
+                this.WindowState = FormWindowState.Normal;
+        }
+
+        
+
+
+
+        
+    }
+}
